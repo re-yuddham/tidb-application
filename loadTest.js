2393ba687e7c66fa28f95f0c2bb5370d66d811f0
DigVijayWa <digvijaywadkar24@gmail.com>
2021-05-29 17:02:45 +0530

    bring the request 5000

--- loadTest.js
+++ loadTest.js
@@ -59,7 +59,7 @@ const options = regionToTest.map((region) => {
     concurrency: 10,
     method: "POST",
     body: generateEmployee(),
-    maxRequests: 100,
+    maxRequests: 5000,
     contentType: "application/json",
   };
 });
